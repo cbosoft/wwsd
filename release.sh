fd480a5cd03a261ebce091cc0f5a69b4b0980191
Chris <chrismjboyle@gmail.com>
2018-06-28 19:09:20 +0100

    Simplified things a bit

--- release.sh
+++ release.sh
@@ -1,14 +1,3 @@
-# Update MD5
-sed -i "17s/.*/md5sums=($(md5sum wwsd | cut -d' ' -f 1))/" PKGBUILD
-git commit PKGBUILD -m "Updated md5sum"
-
-# Increment 'build' number
-echo "$(cat buildno) + 1" | bc > buildno
-git commit buildno -m "Updated 'build' number"
-
-# Push to github
-git push
-
 # Put Updated PKGBUILD onto AUR
 cp PKGBUILD ../wwsd-git/PKGBUILD
 cd ../wwsd-git

1917a36bfe8cc3b1aa987b91e3397ab1771ac3b8
Chris <chrismjboyle@gmail.com>
2018-06-28 23:23:37 +0100

    Updated

--- release.sh
+++ release.sh
@@ -1,6 +1,8 @@
 # Put Updated PKGBUILD and .SRCINFO onto AUR
+makepkg
 cp PKGBUILD ../wwsd-git/PKGBUILD
 cd ../wwsd-git
 makepkg --printsrcinfo > .SRCINFO
+git clean -xdf
 git commit PKGBUILD .SRCINFO
 git push
